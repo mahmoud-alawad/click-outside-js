9d4b9122099109fd18e45131409fc9a848408336
Mahmoud Alawad <76591349+mahmoud-alawad@users.noreply.github.com>
2024-10-25 20:29:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,26 +11,26 @@ class ClickOutside {
         ? [...elementsTriggers]
         : [...document.querySelectorAll('[data-click-outside-trigger]')]
 
-    this.clickOutSideEvents = this.elements?.map(element => {
-      const event = new Event('clickOutSideElement', {
-        bubbles: true,
-        detail: { element: () => element }
-      })
-      element?.dispatchEvent(event)
-      return event
-    })
-
     this.evt = e => {
       if (!this.elements?.length || !this.triggers?.length) {
         return
       }
+
       const element = this.elements?.find(el =>
         el?.classList?.contains('active')
       )
+
       const elementTrigger = this.triggers?.find(el =>
         el?.classList?.contains('active')
       )
+
       if (e.target !== element && e.target !== elementTrigger) {
+        const event = new Event('onClickOutSide', {
+          bubbles: true,
+          detail: { element: () => element }
+        })
+        element?.dispatchEvent(event)
+
         if (typeof this.cb === 'function') {
           this.cb(element, elementTrigger)
         }
